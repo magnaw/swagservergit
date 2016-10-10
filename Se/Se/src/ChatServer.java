8effee906396072e25d9cd346863b14720729a04
sorenkolbyejensen <sorenkolbyejensen@srens-mbp>
2016-10-10 20:25:08 +0200

    Package fix

--- Se/Se/src/ChatServer.java
+++ Se/Se/src/ChatServer.java
@@ -1,4 +1,4 @@
-package tester;
+
 // IO imports
 import java.io.BufferedReader;
 import java.io.IOException;
--- Se/Se/src/Klient.java
+++ Se/Se/src/Klient.java
@@ -1,4 +1,4 @@
-package tester;
+
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
